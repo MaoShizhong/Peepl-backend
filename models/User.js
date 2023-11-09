e5f74adf7952e22a409620ce8183698d2e09ee29
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-11-09 17:07:46 +0000

    Update User model and test setup users
    Change bcrypt for argon2id

--- models/User.js
+++ models/User.js
@@ -1,5 +1,24 @@
 const { model, Schema } = require('mongoose');
 
+const EmploymentSchema = new Schema(
+    {
+        title: String,
+        company: String,
+        start: Date,
+        end: Date,
+    },
+    { _id: false }
+);
+
+const EducationSchema = new Schema(
+    {
+        institution: String,
+        start: Date,
+        end: Date,
+    },
+    { _id: false }
+);
+
 const FriendSchema = new Schema(
     {
         user: { type: Schema.Types.ObjectId, ref: 'user', unique: true, required: true },
@@ -8,27 +27,8 @@ const FriendSchema = new Schema(
     { _id: false }
 );
 
-const UserSchema = new Schema(
+const TokenSchema = new Schema(
     {
-        email: { type: String, required: true },
-        auth: [{ type: String, enum: ['local', 'github'] }],
-        password: {
-            type: String,
-            required: function () {
-                return this.auth === 'local';
-            },
-        },
-        githubID: {
-            type: String,
-            required: function () {
-                return this.auth === 'github';
-            },
-        },
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        DOB: { type: Date, required: true },
-        friends: [FriendSchema],
-        isDemo: { type: Boolean, default: undefined },
         passwordReset: {
             token: String,
             expiry: Date,
@@ -37,10 +37,85 @@ const UserSchema = new Schema(
         accountDeletion: {
             token: String,
             expiry: Date,
+            used: Boolean,
         },
         githubLoginToken: String,
     },
-    { versionKey: false }
+    { _id: false }
 );
+
+const UserSchema = new Schema(
+    {
+        handle: { type: String, required: true },
+        email: { type: String, required: true },
+        auth: {
+            strategies: [{ type: String, enum: ['local', 'github'] }],
+            password: {
+                type: String,
+                required: function () {
+                    return this.strategies.includes('local');
+                },
+            },
+            githubID: {
+                type: String,
+                required: function () {
+                    return this.strategies.includes('github');
+                },
+            },
+        },
+        details: {
+            firstName: { type: String, required: true },
+            lastName: { type: String, required: true },
+            DOB: {
+                value: { type: Date, required: true },
+                visiblity: {
+                    type: String,
+                    enum: ['everyone', 'friends', 'hidden'],
+                    required: true,
+                },
+            },
+            city: {
+                value: { type: Date, required: true },
+                visiblity: {
+                    type: String,
+                    enum: ['everyone', 'friends', 'hidden'],
+                    required: true,
+                },
+            },
+            country: {
+                value: { type: Date, required: true },
+                visiblity: {
+                    type: String,
+                    enum: ['everyone', 'friends', 'hidden'],
+                    required: true,
+                },
+            },
+            employment: {
+                value: [EmploymentSchema],
+                visibility: {
+                    type: String,
+                    enum: ['everyone', 'friends', 'hidden'],
+                    required: true,
+                },
+            },
+            education: {
+                value: [EducationSchema],
+                visibility: {
+                    type: String,
+                    enum: ['everyone', 'friends', 'hidden'],
+                    required: true,
+                },
+            },
+        },
+        friends: [FriendSchema],
+        isDemo: { type: Boolean, default: undefined },
+        tokens: TokenSchema,
+    },
+    { toJSON: { virtuals: true }, versionKey: false }
+);
+
+UserSchema.virtual('name').get(function () {
+    return `${this.details.firstName} ${this.details.lastName}`;
+});
 
 module.exports = model('user', UserSchema);
--- tests/config/test_users.js
+++ tests/config/test_users.js
@@ -4,41 +4,169 @@ exports.users = [
     {
         _id: userIDs[0],
         email: 'user0@test.com',
-        auth: ['local'],
-        password: 'asdfASDF0',
-        firstName: 'First0',
-        lastName: 'Last0',
-        DOB: new Date('1995-12-17T03:24:00'),
+        auth: { strategies: ['local'], password: 'asdfASDF0' },
+        details: {
+            firstName: 'First0',
+            lastName: 'Last0',
+            DOB: {
+                value: new Date('1995-12-17T03:20:00'),
+                visibility: 'everyone',
+            },
+            city: {
+                value: 'Zeroton',
+                visibility: 'everyone',
+            },
+            country: {
+                value: 'United Kingdom',
+                visibility: 'everyone',
+            },
+            employment: {
+                value: [
+                    {
+                        title: '0Title',
+                        company: '0Company',
+                        start: new Date('2015-12-17T03:20:00'),
+                    },
+                ],
+                visibility: 'everyone',
+            },
+            education: {
+                value: [
+                    {
+                        insitution: '0School',
+                        start: new Date('2000-12-17T03:20:00'),
+                        end: new Date('2015-12-17T03:20:00'),
+                    },
+                ],
+                visibility: 'everyone',
+            },
+        },
         friends: [],
     },
     {
         _id: userIDs[1],
         email: 'user1@test.com',
-        auth: ['local'],
-        password: 'asdfASDF1',
-        firstName: 'First1',
-        lastName: 'Last1',
-        DOB: new Date('1995-12-17T03:24:00'),
+        auth: { strategies: ['local'], password: 'asdfASDF1' },
+        details: {
+            firstName: 'First1',
+            lastName: 'Last1',
+            DOB: {
+                value: new Date('1995-12-17T03:21:00'),
+                visibility: 'everyone',
+            },
+            city: {
+                value: 'Oneton',
+                visibility: 'everyone',
+            },
+            country: {
+                value: 'United Kingdom',
+                visibility: 'everyone',
+            },
+            employment: {
+                value: [
+                    {
+                        title: '1Title',
+                        company: '1Company',
+                        start: new Date('2015-12-17T03:21:00'),
+                    },
+                ],
+                visibility: 'everyone',
+            },
+            education: {
+                value: [
+                    {
+                        insitution: '1School',
+                        start: new Date('2000-12-17T03:21:00'),
+                        end: new Date('2015-12-17T03:21:00'),
+                    },
+                ],
+                visibility: 'everyone',
+            },
+        },
         friends: [],
     },
     {
         _id: userIDs[2],
         email: 'user2@test.com',
-        auth: ['local'],
-        password: 'asdfASDF2',
-        firstName: 'First2',
-        lastName: 'Last2',
-        DOB: new Date('1995-12-17T03:24:00'),
+        auth: { strategies: ['local'], password: 'asdfASDF2' },
+        details: {
+            firstName: 'First2',
+            lastName: 'Last2',
+            DOB: {
+                value: new Date('1995-12-17T03:22:00'),
+                visibility: 'friends',
+            },
+            city: {
+                value: 'Twoton',
+                visibility: 'friends',
+            },
+            country: {
+                value: 'United Kingdom',
+                visibility: 'friends',
+            },
+            employment: {
+                value: [
+                    {
+                        title: '2Title',
+                        company: '2Company',
+                        start: new Date('2015-12-17T03:22:00'),
+                    },
+                ],
+                visibility: 'friends',
+            },
+            education: {
+                value: [
+                    {
+                        insitution: '2School',
+                        start: new Date('2000-12-17T03:22:00'),
+                        end: new Date('2015-12-17T03:22:00'),
+                    },
+                ],
+                visibility: 'friends',
+            },
+        },
         friends: [],
     },
     {
         _id: userIDs[3],
         email: 'user3@test.com',
-        auth: ['local'],
-        password: 'asdfASDF3',
-        firstName: 'First3',
-        lastName: 'Last3',
-        DOB: new Date('1995-12-17T03:24:00'),
+        auth: { strategies: ['local'], password: 'asdfASDF3' },
+        details: {
+            firstName: 'First3',
+            lastName: 'Last3',
+            DOB: {
+                value: new Date('1995-12-17T03:20:00'),
+                visibility: 'hidden',
+            },
+            city: {
+                value: 'Threeton',
+                visibility: 'hidden',
+            },
+            country: {
+                value: 'United Kingdom',
+                visibility: 'hidden',
+            },
+            employment: {
+                value: [
+                    {
+                        title: '3Title',
+                        company: '3Company',
+                        start: new Date('2015-12-17T03:23:00'),
+                    },
+                ],
+                visibility: 'hidden',
+            },
+            education: {
+                value: [
+                    {
+                        insitution: '3School',
+                        start: new Date('2000-12-17T03:23:00'),
+                        end: new Date('2015-12-17T03:23:00'),
+                    },
+                ],
+                visibility: 'hidden',
+            },
+        },
         friends: [],
     },
 ];
